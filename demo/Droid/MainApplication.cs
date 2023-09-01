08b0148fc2ddb7b92a6575c5e655963b569de7ae
Sweekriti Satpathy <swsat@microsoft.com>
2023-09-01 16:39:22 -0700

    adding things to old demo

---
+++ demo/Droid/MainApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MapBoxQs.iOS;
+
+namespace MapBoxQs.Droid
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+
+
+}
